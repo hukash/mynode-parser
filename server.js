e305dc57f6c8a8cf169ff84a839558c28273bc2b
Lukas Sobik <lso@rgu.de>
2010-08-30 16:20:57 +0200

    restructured code

---
+++ server.js
@@ -0,0 +1,18 @@
+process.addListener('uncaughtException', function(err, stack){
+  console.log("-----------------");
+  console.log("Exception: " + err);
+  console.log(err.stack);
+  console.log("-----------------");
+});
+
+var ContentParser = require('./lib/contentparser');
+
+new ContentParser({
+  port: 8000,
+  website: {
+      host: 'www.rgu.de'
+    , port: 80
+    , requestMethod: 'GET'
+    , path: '/'
+  }  
+});
